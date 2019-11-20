06cf51e2ccce87d24950cb2f1858db31d7029121
Semyon Danilov <sdanilov@naumen.ru>
2019-11-20 17:23:22 +0300

    typeahead

--- src/App.tsx
+++ src/App.tsx
@@ -1,134 +1,19 @@
-import React from 'react';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link,
-    Redirect,
-    useHistory,
-    useLocation
-} from "react-router-dom";
+import React, {Fragment} from 'react';
+import {Typeahead} from 'react-bootstrap-typeahead';
+import data from "./data";
 
-const routes = [
-    {
-        path: "/login",
-        component: LoginPage
-    },
-    {
-        path: "/public",
-        component: PublicPage
-    },
-    {
-        path: "/protected",
-        render: ({location}: {location: any}) => {
-            return fakeAuth.isAuthenticated ? (
-                <ProtectedPage/>
-            ) : (
-                <Redirect
-                    to={{
-                        pathname: "/login",
-                        state: {from: location}
-                    }}
+export const App: React.FC = props => {
+    return (
+        <div className="container">
+            <Fragment>
+                <Typeahead
+                    labelKey="name"
+                    options={data}
+                    multiple={true}
+                    placeholder="Choose a state..."
                 />
-            )
-        }
-    }
-];
-
-export default function AuthExample() {
-    return (
-        <Router>
-            <div>
-                <AuthButton/>
-
-                <ul>
-                    <li>
-                        <Link to="/public">Public Page</Link>
-                    </li>
-                    <li>
-                        <Link to="/protected">Protected Page</Link>
-                    </li>
-                </ul>
-
-                <Switch>
-                    {routes.map((route, i) => (
-                        <RouteWithSubRoutes key={i} {...route} />
-                    ))}
-                </Switch>
-            </div>
-        </Router>
-    );
-}
-
-function RouteWithSubRoutes(route: any, extraProps = {}) {
-    return (
-        <Route
-            exact={route.exact}
-            path={route.path}
-            render={props => route.render
-                ? route.render(props)
-                : <route.component {...props} {...extraProps} route={route}/>}
-            strict={route.strict}/>
-    );
-}
-
-const fakeAuth = {
-    isAuthenticated: false,
-    async authenticate(cb: (() => void)) {
-        let profile = await fetch("http://localhost:4000/profile");
-        console.log(profile);
-        fakeAuth.isAuthenticated = true;
-        setTimeout(cb, 100);
-    },
-    signout(cb: () => void) {
-        fakeAuth.isAuthenticated = false;
-        setTimeout(cb, 100);
-    }
-};
-
-function AuthButton() {
-    let history = useHistory();
-
-    return fakeAuth.isAuthenticated ? (
-        <p>
-            Welcome!{" "}
-            <button
-                onClick={() => {
-                    fakeAuth.signout(() => history.push("/"));
-                }}
-            >
-                Sign out
-            </button>
-        </p>
-    ) : (
-        <p>You are not logged in.</p>
-    );
-}
-
-
-function PublicPage() {
-    return <h3>Public</h3>;
-}
-
-function ProtectedPage() {
-    return <h3>Protected</h3>;
-}
-
-function LoginPage() {
-    let history = useHistory();
-    let location = useLocation();
-
-    let {from} = location.state || {from: {pathname: "/"}};
-    let login = () => {
-        fakeAuth.authenticate(() => {
-            history.replace(from);
-        });
-    };
-
-    return (
-        <div>
-            <p>You must log in to view the page at {from.pathname}</p>
-            <button onClick={login}>Log in</button>
+            </Fragment>
         </div>
-    );
-}
+    )
+};
+export default App;
